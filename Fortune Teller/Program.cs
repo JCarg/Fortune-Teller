44d052263e5ad4243262f4bc422ba5854b3dcf27
JCarg <Jcarg108@gmail.com>
2017-01-06 15:24:22 -0500

    Adding Questions

---
+++ Fortune Teller/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortune_Teller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the most accurate virtual fortune telling system on the web.");
+            Console.WriteLine("Let's Begin!\a");
+            Console.WriteLine("What is your first name? Write below.");
+            string fname=Console.ReadLine();
+            Console.WriteLine("What is your last name? Write below.");
+            string lname = Console.ReadLine();
+            Console.WriteLine("What is your age?Write below.");
+
+        }
+    }
+}
