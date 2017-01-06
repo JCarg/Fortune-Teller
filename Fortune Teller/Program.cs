9bf9848b0f3471dfadb5f981878311c88a580da8
JCarg <Jcarg108@gmail.com>
2017-01-06 17:38:25 -0500

    Finished name and age sections

--- Fortune Teller/Program.cs
+++ Fortune Teller/Program.cs
@@ -12,11 +12,42 @@ namespace Fortune_Teller
         {
             Console.WriteLine("Welcome to the most accurate virtual fortune telling system on the web.");
             Console.WriteLine("Let's Begin!\a");
-            Console.WriteLine("What is your first name? Write below.");
-            string fname=Console.ReadLine();
-            Console.WriteLine("What is your last name? Write below.");
-            string lname = Console.ReadLine();
-            Console.WriteLine("What is your age?Write below.");
+
+
+            Console.WriteLine("What is your first name? Type answers below.");
+            string firstname=Console.ReadLine();
+            Console.WriteLine("\nWhat is your last name?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("\nWhat is your age?");
+            int age = int.Parse(Console.ReadLine());
+            short oddretireyears = 800;
+            byte evenretireyears = 5;
+            if (age%2>0)
+            {
+                Console.WriteLine("You will retire in " + oddretireyears + " years.");
+            }      
+            else
+            {
+                Console.WriteLine("You will retire in " + evenretireyears + " years.");
+            }
+
+
+            Console.WriteLine("What is your birth month? Ex: October = 10");
+            byte birthmonth = byte.Parse(Console.ReadLine());
+            int janaprilcash = 10000000;
+            if (birthmonth>=1&&birthmonth<=4)
+
+
+            Console.WriteLine("What is your favorite ROYGBIV color?");
+            Console.WriteLine("If you would a list of ROYGBIV colors type help.");
+            string color = Console.ReadLine();
+            string help = color.ToLower();
+            Console.WriteLine(color);
+            if (help == "help")
+            {
+                Console.Write("\nRed\nOrange\nYellow\nGreen\nBlue\nIndigo\nViolet\n");
+            }
 
         }
     }
